514e5020c7a9f7a3c5a297cff98401b2c77e5d65
subbu <subbu@gmail.com>
2019-11-01 15:42:55 +0000

    Changed fremework to POM

--- src/test/java/stepDefinitions/Steps.java
+++ src/test/java/stepDefinitions/Steps.java
@@ -1,21 +1,25 @@
 package stepDefinitions;
 
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.By;
+import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
+import pageObjects.CartPage;
+import pageObjects.CheckoutPage;
+import pageObjects.HomePage;
+import pageObjects.ProductListingPage;
 
 public class Steps {
 	
 	WebDriver driver;
-	
+	HomePage home;
+	ProductListingPage productListingPage;
+	CartPage cartPage;
+	CheckoutPage checkoutPage;
+	 
 	@Given("^user is on Home Page$")
 	public void user_is_on_Home_Page() throws Throwable {
 		System.setProperty("webdriver.chrome.driver","C:\\Libs\\chromedriver.exe");
@@ -25,83 +29,60 @@ public class Steps {
 		 driver.get("http://www.shop.demoqa.com");
 	}
 
-	@When("^he search for dress$")
-	public void he_search_for_dress() throws Throwable {
-		driver.navigate().to("http://shop.demoqa.com/?s=" + "dress" + "&post_type=product");
-	}
+	 @When("^he search for \"([^\"]*)\"$")
+	 public void he_search_for(String product)  {
+		 HomePage home = new HomePage(driver);
+		 home.perform_Search(product);
+	 }
 
 	@When("^choose to buy the first item$")
 	public void choose_to_buy_the_first_item() throws Throwable {
 		
-		 List<WebElement> items = driver.findElements(By.cssSelector(".noo-product-inner"));
-		 items.get(0).click();
+		ProductListingPage productListingPage = new ProductListingPage(driver);
+		 productListingPage.select_Product(0);
 		 
 		 JavascriptExecutor js = (JavascriptExecutor) driver;
 		 js.executeScript("window.scrollBy(0,500)");
 		 
-		 Thread.sleep(10000);
-		 Select dropdowncolor = new Select(driver.findElement(By.id("pa_color"))); 
-		 dropdowncolor.selectByIndex(1);
+		 productListingPage.clickOn_addColor();
+		 productListingPage.clickOn_Addsize();
 		 
-		 Thread.sleep(10000);
-		 Select dropdownsize = new Select(driver.findElement(By.id("pa_size"))); 
-		 dropdownsize.selectByIndex(1);
-		 
-		 Thread.sleep(10000);
-		 WebElement addToCart = driver.findElement(By.cssSelector("button.single_add_to_cart_button"));
-		 addToCart.click();
+		 productListingPage.clickOn_AddToCart(); 
 		 
 	}
 
 	@When("^moves to checkout from mini cart$")
 	public void moves_to_checkout_from_mini_cart() throws Throwable {
 		
-		WebElement cart = driver.findElement(By.cssSelector(".cart-button"));
-		cart.click();
-		 
-		WebElement continueToCheckout = driver.findElement(By.cssSelector(".checkout-button.alt"));
-		continueToCheckout.click(); 
+		CartPage cartPage = new CartPage(driver);
+		cartPage.clickOn_Cart();
+		cartPage.clickOn_ContinueToCheckout(); 
+		
 	}
 
 	@When("^enter personal details on checkout page$")
 	public void enter_personal_details_on_checkout_page() throws Throwable {
 		
-		Thread.sleep(5000);
-		 WebElement firstName = driver.findElement(By.cssSelector("#billing_first_name"));
-		 firstName.sendKeys("Lakshay");
-		 
-		 WebElement lastName = driver.findElement(By.cssSelector("#billing_last_name"));
-		 lastName.sendKeys("Sharma");
-		 
-		 WebElement street = driver.findElement(By.id("billing_address_1"));
-		 street.sendKeys("2nd street");
-		 
-		 WebElement town = driver.findElement(By.id("billing_city"));
-		 town.sendKeys("London");
-		 
-		 WebElement postcode = driver.findElement(By.cssSelector("#billing_postcode"));
-		 postcode.sendKeys("RM11 2NP"); 
-		 
-		 WebElement emailAddress = driver.findElement(By.cssSelector("#billing_email"));
-		 emailAddress.sendKeys("test@gmail.com");
-		 
-		 WebElement phone = driver.findElement(By.cssSelector("#billing_phone"));
-		 phone.sendKeys("07438862327");
-		 
+		
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0,300)");
+		
+		checkoutPage = new CheckoutPage(driver);
+		checkoutPage.fill_PersonalDetails(); 
 	}
 
 	@When("^accept terms and conditions$")
-	public void select_same_delivery_address() throws Throwable {
+	public void accept_terms_conditions() throws Throwable {
 	    
-		 WebElement acceptTC = driver.findElement(By.cssSelector("#terms.input-checkbox"));
-		 acceptTC.click();
+		checkoutPage.check_TermsAndCondition(true);
 	}
 
 	@When("^place the order$")
 	public void place_the_order() throws Throwable {
 	    
-		 WebElement placeOrder = driver.findElement(By.cssSelector("#place_order"));
-		 placeOrder.submit();
+		checkoutPage.clickOn_PlaceOrder();
+		driver.quit();
 	}
 
 }
--- src/test/resources/functionalTests/End2End_Test.feature
+++ src/test/resources/functionalTests/End2End_Test.feature
@@ -3,7 +3,7 @@ Description: The purpose of this feature is to test End 2 End integration
 
 Scenario: Customer place an order by purchasing an item from search
 	Given user is on Home Page
-	When he search for dress
+	When he search for "dress"
 	And choose to buy the first item
 	And moves to checkout from mini cart
 	And enter personal details on checkout page
